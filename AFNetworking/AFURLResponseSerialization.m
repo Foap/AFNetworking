8c18b4aa51358e1847030cd87d77a31e5d3e42a4
mrupala <marek@appnroll.com>
2021-04-14 16:32:57 +0200

    Serializer supplement.

--- AFNetworking/AFURLResponseSerialization.m
+++ AFNetworking/AFURLResponseSerialization.m
@@ -670,7 +670,23 @@ static UIImage * AFInflatedImageFromResponseWithDataAtScale(NSHTTPURLResponse *r
 
 //    self.acceptableContentTypes = [[NSSet alloc] initWithObjects:@"image/tiff", @"image/jpeg", @"image/gif", @"image/png", @"image/ico", @"image/x-icon", @"image/bmp", @"image/x-bmp", @"image/x-xbitmap", @"image/x-win-bitmap", nil];
     
-    self.acceptableContentTypes =  [[NSSet alloc] initWithObjects:@"image/tiff", @"image/jpeg", @"image/jpg" @"image/gif", @"image/png", @"image/ico", @"image/x-icon", @"image/bmp", @"image/x-bmp", @"image/x-xbitmap", @"image/x-win-bitmap", @"image/webp", @"binary/octet-stream", @"application/octet-stream", @"image/heic", @"image/heic-sequence", nil];
+    self.acceptableContentTypes =  [[NSSet alloc] initWithObjects:@"image/tiff",
+                                                                  @"image/jpeg",
+                                                                  @"image/jpg",
+                                                                  @"image/gif",
+                                                                  @"image/png",
+                                                                  @"image/ico",
+                                                                  @"image/x-icon",
+                                                                  @"image/bmp",
+                                                                  @"image/x-bmp",
+                                                                  @"image/x-xbitmap",
+                                                                  @"image/x-win-bitmap",
+                                                                  @"image/webp",
+                                                                  @"binary/octet-stream",
+                                                                  @"application/octet-stream",
+                                                                  nil];
+    
+    
     
     NSArray *array = [self.acceptableContentTypes allObjects];
     NSLog(@"ContentTypes: %@",array);
